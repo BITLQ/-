3a5863c92b8ff22a06a171358d1a56de5360b9cd
Johnxs <849886839@qq.com>
2017-07-16 17:17:10 +0800

    cgi shell 的一些目录结构正确显示bug修复

--- http_protect_2017_7_10/dir_cgi.sh
+++ http_protect_2017_7_10/dir_cgi.sh
@@ -3,8 +3,8 @@
 DIR_PATH=${DIR_ENV}
 
 array=$(ls ${DIR_PATH})
-DIR_PATH=${DIR_PATH:7}
+DIR_PATH=${DIR_PATH:8}
 for i in ${array[*]}
 do
-    echo "<a href="http://${DIR_PATH}${i}/">${i}</a><br/>"
+    echo "<a href="${DIR_PATH}/${i}">${i}</a><br/>"
 done
